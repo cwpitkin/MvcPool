e174275077f03c1920f95cfb6b207c65af354946
cwpitkin <cwpitkin@gmail.com>
2016-01-10 22:59:31 -0800

    Added db project with inital Users table

---
+++ MvcPoolDb/dbo/Tables/Users.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserId]    INT        NOT NULL,
+    [FirstName] NVARCHAR(50) NULL,
+    [LastName]  NVARCHAR(50) NULL,
+    [UserName]  NVARCHAR(50) NOT NULL,
+    [CountryId] INT        NULL,
+    [PasswordHash] NVARCHAR(MAX) NOT NULL, 
+    [Email] NCHAR(256) NOT NULL, 
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
